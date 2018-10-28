030a22d4509e6d9cbecc9b4484c5e67f19df184c
Arthur Villar <=>
2018-10-28 16:30:53 -0400

    Fix apps menu on admin page

--- resources/views/admin/components/apps.blade.php
+++ resources/views/admin/components/apps.blade.php
@@ -5,34 +5,34 @@
 
     <div class="dropdown-menu dropdown-menu-right dropdown-menu--block" role="menu">
         <div class="row app-shortcuts px-4 py-2">
-            <a class="col-4 app-shortcuts__item link-none" href="https://s3.console.aws.amazon.com/s3/home?region=us-east-1#" target="_blank">
+            <a class="col-4 app-shortcuts__item link-none" href="https://s3.console.aws.amazon.com/s3/home?region=us-east-1#" title="We use Amazon S3 to store images and videos." target="_blank">
                 <i class="fab fa-aws"></i>
                 <small class="text-muted t-2">Amazon S3</small>
                 <span class="app-shortcuts__helper bg-red"></span>
             </a>
-            <a class="col-4 app-shortcuts__item link-none" href="https://dashboard.stripe.com/test/dashboard" target="_blank">
+            <a class="col-4 app-shortcuts__item link-none" href="https://dashboard.stripe.com/test/dashboard" title="We use Stripe to hangle online payments." target="_blank">
                 <i class="fab fa-stripe"></i>
                 <small class="text-muted t-2">Stripe</small>
                 <span class="app-shortcuts__helper bg-blue"></span>
             </a>
-            <a class="col-4 app-shortcuts__item link-none" href="https://anandamayiyoga.createsend.com/" target="_blank">
+            <a class="col-4 app-shortcuts__item link-none" href="https://anandamayiyoga.createsend.com/" title="We use Campaign Monitor to handle marketing emails." target="_blank">
                 <i class="fas fa-at"></i>
                 <small class="text-muted t-2">C.Monitor</small>
                 <span class="app-shortcuts__helper bg-orange"></span>
             </a>
-            <a class="col-4 app-shortcuts__item link-none" href="https://disqus.com/" target="_blank">
+            <a class="col-4 app-shortcuts__item link-none" href="https://disqus.com/" title="We use DISQUS to provide comments on videos and programs." target="_blank">
                 <i class="fas fa-comments"></i>
                 <small class="text-muted t-2">Disqus</small>
                 <span class="app-shortcuts__helper bg-teal"></span>
             </a>
-            <a class="col-4 app-shortcuts__item link-none" href="https://painel-email.locaweb.com.br/domains/anandamayiyoga.com/mailboxes" target="_blank">
+            <a class="col-4 app-shortcuts__item link-none" href="https://sogo3.gandi.net/SOGo/so/" title="Open the webmail on Gandi.net" target="_blank">
                 <i class="fas fa-envelope-open"></i>
                 <small class="text-muted t-2">Webmail</small>
                 <span class="app-shortcuts__helper bg-light-green"></span>
             </a>
-            <a class="col-4 app-shortcuts__item link-none" href="https://painelhospedagem.locaweb.com.br/databases/65504776?ticket=ST-884231-jJTmEqzanotgMultXGDW-nina-node2" target="_blank">
-                <i class="fas fa-database"></i>
-                <small class="text-muted t-2">Database</small>
+            <a class="col-4 app-shortcuts__item link-none" href="https://cloud.digitalocean.com/login" title="Log into Digital Ocean" target="_blank">
+                <i class="fab fa-digital-ocean"></i>
+                <small class="text-muted t-2">Server</small>
                 <span class="app-shortcuts__helper bg-blue-grey"></span>
             </a>
         </div>
