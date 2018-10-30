8630a593b64e7461f91434010b765fc203797120
Arthur Villar <=>
2018-10-30 14:36:17 -0400

    Implement DISQUS on reads

--- resources/views/pages/reads/blog/show/index.blade.php
+++ resources/views/pages/reads/blog/show/index.blade.php
@@ -45,7 +45,27 @@
     		</div>
     	</div>
     </div>
-
+    @include('components/disqus/show')
 </div>
 
+@endsection
+
+@section('scripts')
+<script>
+/**
+*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+/*
+var disqus_config = function () {
+this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+*/
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://anandamayiyoga.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
 @endsection
