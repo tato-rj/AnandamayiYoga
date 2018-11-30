68936a24649ec5dfe6a7225a66f045099ef5a28f
Arthur Villar <=>
2018-11-30 04:01:04 -0500

    Change plants from png to svg

--- resources/views/pages/welcome/level-card.blade.php
+++ resources/views/pages/welcome/level-card.blade.php
@@ -1,10 +1,12 @@
 <div class="col-lg-4 col-md-4 col-sm-6 col-8 mx-auto move-control p-4 text-center">
-    <div class="cursor-pointer plant-container position-relative mx-auto mb-2" data-id="1" data-toggle="modal" data-target="#classes">
-        <div class="sun-{{$level->slug}} sun rounded-circle">
-            <div></div>
-        </div>
-        <div class="level-card plant plant-{{$level->slug}}" style="background: url({{cloud('app/images/levels/'.$level->slug.'-sprite.png')}}) left center; background-size: cover;" ></div>
-        <img src="{{cloud('app/images/levels/soil.png')}}" class="position-absolute soil">
+    <div class="cursor-pointer level-card" data-id="1" data-toggle="modal" data-target="#classes">
+    	<div class="position-relative mx-auto plant-container mb-2">
+	        <div class="sun-{{$level->slug}} sun rounded-circle">
+	            <div></div>
+	        </div>
+	        <div class="level-card plant plant-{{$level->slug}}" style="background: url({{cloud('app/images/levels/'.$level->slug.'.svg')}}) left center; background-size: cover;" ></div>
+	        <img src="{{cloud('app/images/levels/soil.svg')}}" class="position-absolute soil">
+	    </div>
     </div>
     <h4 class="text-muted">{{$level->name}}</h4>
 </div>
