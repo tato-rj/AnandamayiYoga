bc4626fd2c351e6dddbb420209e3ef1c59696cbe
Arthur Villar <=>
2019-04-20 19:36:03 -0400

    Fix temp registration bug

--- resources/views/components/temp.blade.php
+++ resources/views/components/temp.blade.php
@@ -1,14 +1,14 @@
 <div class="col-lg-5 col-md-6 col-10 mx-auto my-5">
 
 <div class="text-center border px-3 py-4">
-	<h5 class="text-red">Os vídeos estarão disponíveis em breve!</h5>
-	<p>Gostaria de receber uma mensagem quando este conteúdo estiver pronto?</p>
+	<h5 class="text-red">@lang('Our videos will be available soon!')</h5>
+	<p>@lang('Would you like to get a notification when our content is ready?')</p>
 	<form method="POST" action="{{route('notify')}}">
 		@csrf
 		<div class="form-group">
 			<input type="email" name="notify_email" required placeholder="Email" class="form-control">
 		</div>
-		<button class="btn btn-red btn-block">Sim, me avise quando estiver pronto!</button>
+		<button class="btn btn-red btn-block">@lang('Yes, let me know!')</button>
 	</form>
 </div>
 
