0731ec64a5c04e764429b8476d8b3ea60502b6a1
Arthur Villar <=>
2019-05-13 10:04:42 -0400

    Fix asanas bug

--- resources/views/pages/discover/asanas/show.blade.php
+++ resources/views/pages/discover/asanas/show.blade.php
@@ -1,27 +1,19 @@
-{{-- @if($asanas->count() > 0)
+@if($asanas->count() > 0)
 <p class="m-0 text-muted"><small>@lang('Total of') <span id="asanas-count">{{$asanas->total()}}</span> asanas</small></p>
-@endif --}}
+@endif
 <div id="asanas-container" class="d-flex align-items-center mt-2 flex-wrap w-100">
-	@if($asanas->count() >= 60)
-	    @forelse($asanas as $asana)
 
-	        @include('components/asana/card')
+    @forelse($asanas as $asana)
 
-	    @empty
-	    
-	        @include('components/filters/empty')
+        @include('components/asana/card')
 
-	    @endforelse
-    @else
-		<div class="text-center w-100 mt-7 mb-4">
-		    <img src="{{cloud('app/brand/logo-gray.svg')}}" 
-		    	class="mb-5" 
-				style="width: 240px; opacity: 0.2;">
-		    <h3
-		    	style="color: lightgrey">@lang('Coming out soon...')</h3>
-		</div>
-    @endif
-{{--     <div class="d-flex align-items-center w-100 justify-content-center my-4">
+    @empty
+    
+        @include('components/filters/empty')
+
+    @endforelse
+
+    <div class="d-flex align-items-center w-100 justify-content-center my-4">
     {{ $asanas->links() }}    
-    </div> --}}
+    </div>
 </div>
