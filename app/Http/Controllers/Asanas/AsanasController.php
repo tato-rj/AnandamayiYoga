29d414ed63c483404410b6102e80609713957667
Arthur Villar <=>
2019-05-13 10:02:47 -0400

    Fix asanas bug

--- app/Http/Controllers/Asanas/AsanasController.php
+++ app/Http/Controllers/Asanas/AsanasController.php
@@ -18,7 +18,7 @@ class AsanasController extends Controller
      */
     public function index(Request $request, AsanaFilters $filters)
     {
-        $asanas = Asana::paginate(20);
+        $asanas = Asana::paginate(20)->get();
         return $asanas;
         return view('pages/discover/asanas/index', compact('asanas'));
     }
