5ed90e4f3a632087b4f3faa1fe30b169ea5aa0f8
Arthur Villar <=>
2019-05-28 11:06:40 -0400

    Return article lead image to original

--- resources/views/pages/reads/articles/show/lead.blade.php
+++ resources/views/pages/reads/articles/show/lead.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-	<section class="col-12 bg-full d-flex align-items-end" style="background-image:url({{cloud($article->image_path)}}); background-position-y: -100px; height: 80vh;">
+	<section class="col-12 bg-full d-flex align-items-end" style="background-image:url({{cloud($article->image_path)}}); background-position: center; height: 60vh;">
 		<div class="overlay w-100 h-100 bg-dark z-0"></div>
 	</section>
 </div>
